0e9597626255e3ef4689f9ff18bb63dee3849ef0
CARLOS CASTILLO BARRADAS <zS23014088@estudiantes.uv.mx>
2025-09-21 16:47:04 -0600

    Refactor: A 3-layer architecture and MVVM pattern are implemented.

---
+++ Lottery.UI/View/UserRegisterView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Lottery.UI.ViewModel;
+
+namespace Lottery.UI.View
+{
+    /// <summary>
+    /// Lógica de interacción para UserRegister.xaml
+    /// </summary>
+    public partial class UserRegister : Window
+    {
+        public UserRegister()
+        {
+            InitializeComponent();
+            this.DataContext = new UserRegisterViewModel();
+        }
+    }
+}
